d014af1f55b4e2f9bf08c5703ed71bb9181a439e
AlexL <chuiflame@gmail.com>
2024-03-06 13:09:43 +0200

    настройка security для получения вебхуков, реализация метода для получения id пользоваетяля и сообщения что он отправил

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -3,8 +3,41 @@
 namespace App\Telegram;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
+use DefStudio\Telegraph\DTO\User;
 
 class Handler extends WebhookHandler
 {
+    public function hello(string $name): void
+    {
+        $this->reply(message: "Привет, $name!");
+    }
+
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        if($text->value() === '/start') {
+            $this->reply(message: 'Я готов к работе!');
+        } else {
+            $this->reply(message: 'Неизвестная команда');
+        }
+    }
+    protected function handleChatMessage(Stringable $text): void
+    {
+        $telegramUserData = $this->message?->from() ?->toArray();
+
+        if ($telegramUserData) {
+            // Создаем объект пользователя с использованием статического метода fromArray
+            $user = User::fromArray($telegramUserData);
+
+            // Получаем ID пользователя
+            $userId = $user->id();
+
+            // Отправляем ответ
+            $this->reply("Ваш Telegram ID: $userId, сообщение: $text");
+        } else {
+            // Обработка ситуации, когда данные о пользователе недоступны
+            $this->reply("Не удалось получить информацию о пользователе");
+        }
+    }
 
 }
